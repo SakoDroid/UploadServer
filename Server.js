f5862139c8ba453f6e63165baaedd119aee49ded
Reza Fatahi <rezagames78@gmail.com>
2020-10-20 20:47:31 +0330

    adding style

--- Server.js
+++ Server.js
@@ -1,12 +1,15 @@
 const ex = require("express");
 const fu = require("express-fileupload");
 const fs = require("fs");
+const b = require("body-parser");
 const app = ex();
 
 const date = new Date();
 
 app.use(fu({limits: { fileSize: 50 * 1024 * 1024 }}));
-
+app.use(ex.static(__dirname + "/doc/"));
+app.use(b.urlencoded({extended : false}));
+let pr = 0;
 app.get("/",(req,res)=>{
     console.log("request for main page from " + req.ip.split(":")[3] + "| " + date.toString());
     res.sendFile(__dirname + "/index.html");
@@ -31,11 +34,11 @@ app.post("/Upload",(req,res)=>{
                "</p><a href='/doc/" + files[i].name + "'>Online view" + "</a>" + "<p>   </p>" +
                "<a href='/dl?file_name=" + encodeURIComponent(files[i].name) +"'>    Download link" + "</a>"+ "<br/><hr/>";
 
-           app.get("/doc/" + files[i].name,(req,res)=>{
+           /*app.get("/doc/" + files[i].name,(req,res)=>{
                console.log("request for online view of the " + dir + " , from : " + req.ip.split(":")[3]
                    + "| " + date.toString());
                res.sendFile(dir);
-           });
+           });*/
        }
        o += "</body></html>";
        res.send(o);
@@ -55,14 +58,20 @@ app.get("/dl",(req,res)=>{
 });
 
 app.get("/files",(req,res)=>{
-    console.log("request for files list from : " + req.ip.split(":")[3]  + "| " + date.toString());
-    let files = fs.readdirSync(__dirname + "/doc");
-    let o = "<html><head><title>Files list</title></head><body><h1>Files list</h1><hr/>";
-    for (let i in files){
-        o += "<a href='" + "/dl?file_name=" + encodeURIComponent(files[i]) + "'>" + files[i] + "</a><br/><hr/>";
+    if(pr === 1){
+        console.log("request for files list from : " + req.ip.split(":")[3] + "| " + date.toString());
+        let files = fs.readdirSync(__dirname + "/doc");
+        let o = "<html><head><title>Files list</title></head><body><h1>Files list</h1><hr/>";
+        let j = 0;
+        for (let i in files) {
+            j++;
+            o += "<a href='" + "/dl?file_name=" + encodeURIComponent(files[i]) + "'>" + j + ". " + files[i] + "</a><br/><hr/>";
+        }
+        o += "</body></html>";
+        res.send(o);
+    }else{
+        res.send("<html><head><title>Access Denied!</title></head><body><h1>Access Denied!</h1></body></html>")
     }
-    o += "</body></html>";
-    res.send(o);
 });
 
 app.listen(8081,()=>{
